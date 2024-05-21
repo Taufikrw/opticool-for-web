7b68a7abb020bb198a90f55b27c0cf3080af54d0
Kipotzzz <124210031@student.upnyk.ac.id>
2024-05-21 21:09:20 +0700

    update user db

--- app/models/users.py
+++ app/models/users.py
@@ -6,6 +6,8 @@ class Users(db.Model):
     name = db.Column(db.String(250), nullable = False)
     email = db.Column(db.String(250), index = True, unique = True, nullable = False)
     password = db.Column(db.String(60), nullable = False)
+    gender = db.Column(db.String(20), nullable = False)
+    birthday = db.Column(db.Date, nullable = False)
     created_at = db.Column(db.DateTime, default = datetime.utcnow)
     updated_at = db.Column(db.DateTime, default = datetime.utcnow)
 
